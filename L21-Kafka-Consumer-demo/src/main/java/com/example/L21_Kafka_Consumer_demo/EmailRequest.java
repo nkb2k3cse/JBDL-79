dcfeb24082279bf16e5aaedf2babbef1dc9aefff
sksqr <shashikant3108@gmail.com>
2025-02-16 19:38:02 +0530

    L-21 Code:KafkaSetup,CoreConcepts,Producer,Consumer,JavaMailSender

---
+++ L21-Kafka-Consumer-demo/src/main/java/com/example/L21_Kafka_Consumer_demo/EmailRequest.java
@@ -0,0 +1,57 @@
+package com.example.L21_Kafka_Consumer_demo;
+
+
+public class EmailRequest {
+
+    private String toEmail;
+
+    private String subject;
+
+    private String body;
+
+    private String cc;
+
+    public EmailRequest(String toEmail, String subject, String body, String cc) {
+        this.toEmail = toEmail;
+        this.subject = subject;
+        this.body = body;
+        this.cc = cc;
+    }
+
+    public EmailRequest() {
+    }
+
+    public String getToEmail() {
+        return toEmail;
+    }
+
+    public void setToEmail(String toEmail) {
+        this.toEmail = toEmail;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getCc() {
+        return cc;
+    }
+
+    public void setCc(String cc) {
+        this.cc = cc;
+    }
+}
+
+
---
+++ L21-Kafka-demo/src/test/java/com/example/L21_Kafka_demo/L21KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.L21_Kafka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L21KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
